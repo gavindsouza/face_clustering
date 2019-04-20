1880dd69de764eaee7aae97ebbccddd801cdf7bc
Gavin Dsouza <gavin18d@gmail.com>
2019-04-20 13:05:30 +0530

    [CHANGES] "class"ified pipeline

--- face_clustering.py
+++ face_clustering.py
@@ -5,34 +5,52 @@ from multiprocessing import cpu_count as num_jobs
 
 # imports - third party imports
 import cv2
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import DBSCAN, KMeans, MeanShift
 from imutils import build_montages
 import numpy as np
 
 
-data_path = r"/mnt/FOURTH/data/kaggle/faces-data/"
-faces = []
+class Model:
+	def __init__(self, algorithm: str = "dbscan"):
+		choices = {
+			"dbscan" : DBSCAN(metric="euclidean", n_jobs=num_jobs()),
+			"kmeans" : KMeans(precompute_distances='auto', n_jobs=num_jobs()),
+			"meanshift" : MeanShift()
+		}
+		self.clt = choices[algorithm]
 
 
-print("[INFO] loading encodings...")
-data = pickle.loads(open("faces_hog.pickle", "rb").read())
-encodings = [row["encoding"] for row in data]
+	def load_data(self, db_path: str = "faces.pickle"):
+		file = open(db_path, "rb").read()
+		self.data = pickle.loads(file)
+		self.encodings = [row["encoding"] for row in self.data]
+		print("[INFO] Encodings Loaded!")
 
-# cluster the embeddings
-print("[INFO] clustering...")
-clt = DBSCAN(metric="euclidean", n_jobs=num_jobs())
-clt.fit(encodings)
 
-labels = [(label, whole_data) for label, whole_data in zip(clt.labels_, data)]
+	def predict(self, enc: list=None): #, db_path: str = "faces.pickle"):
+		if enc is None:
+			enc = self.encodings
 
-with open("final.csv", "w") as f:
-	for label in labels:
-		label, whole_data = label
+		print("[INFO] clustering...")
 		
-		img_path = whole_data["img_path"],
-		time_stamp = whole_data["time_stamp"],
-		box_loc = whole_data["box_loc"],
-		encoding = whole_data["encoding"],
-		numeric_label = label
-        
-		f.write(f"{img_path},{box_loc},{time_stamp},{numeric_label}\n")
+		self.clt.fit(enc)
+		self.predicted_labels = [(label, whole_data) for label, whole_data in zip(self.clt.labels_, self.data)]
+
+		return self.predicted_labels
+
+	
+	def save_data(self, labels: list = None, filename: str = "results"):
+		if labels is None:
+			labels = self.predicted_labels
+
+		with open(f"{filename}.csv", "w") as f:
+			for label_ in labels:
+				label, whole_data = label_
+
+				img_path = whole_data["img_path"],
+				time_stamp = whole_data["time_stamp"],
+				box_loc = whole_data["box_loc"],
+				encoding = whole_data["encoding"],
+				numeric_label = label
+
+				f.write(f"{img_path},{box_loc},{time_stamp},{numeric_label}\n")
